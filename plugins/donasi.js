6803cff946f09b95c7bdb4f6de2d2383c3201479
Mr Yaz 02 <andrias.korex@gmail.com>
2022-11-20 15:21:40 +0000

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,24 +4,16 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //let pp = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image')
 
 let str = `${global.wm}
-
-┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
-❏────
-
-┌─「 Donasi • Non Pulsa 」
+┌─「 Donasi  」
 │ • *Dana:* [${global.pdana}]
-│ • *Gopay:* [${global.pgopay}]
-│ • *Ovo:* [${global.povo}]
-│ • *Link Aja:* [${global.plinkaja}]
 ❏────`
 let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/andri.yoo",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'YAZBOT V4 MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig
